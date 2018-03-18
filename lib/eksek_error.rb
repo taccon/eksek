6c9f82e86da98fdbaefe735613863def6fa68ba2
Johnny Lee-Othon <jleeothon@icloud.com>
2018-03-18 23:06:27 +0100

    Restructure everything 🌋

---
+++ lib/eksek_error.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Represents an error in the executio of a system call when success! was
+# required.
+class EksekError < StandardError
+end
--- spec/eksek_spec.rb
+++ spec/eksek_spec.rb
@@ -1,16 +1,38 @@
+# frozen_string_literal: true
+
 require 'eksek'
 
-RSpec.describe Eksek, '#method_missing' do
-  it 'returns true or false for :success?' do
+RSpec.describe Eksek, '#success?' do
+  it 'returns true or false' do
     expect(Eksek.success?('true')).to be(true)
     expect(Eksek.success?('exit 1')).to be(false)
   end
+end
 
+RSpec.describe Eksek, '#success!' do
   it 'fails on :success! when appropriatly' do
     expect { Eksek.success!('true') }.not_to raise_error
     expect { Eksek.success!('exit 1') }.to raise_error EksekError
   end
+end
 
+RSpec.describe Eksek, '#exit' do
+  it 'returns the exit code' do
+    expect(Eksek.exit('exit 0')).to be(0)
+    expect(Eksek.exit('exit 1')).to be(1)
+    expect(Eksek.exit('exit 7')).to be(7)
+  end
+end
+
+RSpec.describe Eksek, '#respond_to?' do
+  it 'recognizes all methods and combinations' do
+    expect(Eksek.respond_to?(:stdout)).to be(0)
+    expect(Eksek.respond_to?(:stdout)).to be(0)
+    expect(Eksek.respond_to?(:stdout)).to be(0)
+  end
+end
+
+RSpec.describe Eksek, '#method_missing' do
   it 'responds to combinations of :stdout/:stderr' do
     expect(Eksek.stdout('echo Hello')).to eq('Hello')
     expect(Eksek.stderr('echo Hello >&2')).to eq('Hello')
@@ -20,12 +42,6 @@ RSpec.describe Eksek, '#method_missing' do
 
     errout = Eksek.stderr_stdout('echo Hello; echo World >&2')
     expect(errout).to eq(%w[World Hello])
-  end
-
-  it 'returns the exit code with :exit' do
-    expect(Eksek.exit('exit 0')).to be(0)
-    expect(Eksek.exit('exit 1')).to be(1)
-    expect(Eksek.exit('exit 7')).to be(7)
   end
 
   it 'accepts a block where the stdin can be written to' do
