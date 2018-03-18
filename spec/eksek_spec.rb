d73ee9cf0e618c57cf9b149a997f1316354f5aa9
Johnny Lee-Othon <jleeothon@icloud.com>
2018-03-18 23:21:40 +0100

    MOar tests

--- spec/eksek_spec.rb
+++ spec/eksek_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'tempfile'
+
 require 'eksek'
 
 RSpec.describe Eksek, '#success?' do
@@ -25,15 +27,29 @@ RSpec.describe Eksek, '#exit' do
 end
 
 RSpec.describe Eksek, '#respond_to?' do
-  it 'recognizes all methods and combinations' do
-    expect(Eksek.respond_to?(:stdout)).to be(0)
-    expect(Eksek.respond_to?(:stdout)).to be(0)
-    expect(Eksek.respond_to?(:stdout)).to be(0)
+  it 'recognizes all basic methods' do
+    expect(Eksek.respond_to?(:stdout)).to be(true)
+    expect(Eksek.respond_to?(:stderr)).to be(true)
+    expect(Eksek.respond_to?(:stdouterr)).to be(true)
+    expect(Eksek.respond_to?(:exit)).to be(true)
+    expect(Eksek.respond_to?(:success?)).to be(true)
+    expect(Eksek.respond_to?(:success!)).to be(true)
+  end
+
+  it 'recognizes some combinations of methods' do
+    expect(Eksek.respond_to?(:stdout_stderr_exit_success!)).to be(true)
+    expect(Eksek.respond_to?(:stdout_stderr_exit_success?)).to be(true)
+    expect(Eksek.respond_to?(:stdouterr_exit_success!)).to be(true)
+    expect(Eksek.respond_to?(:stdouterr_exit_success?)).to be(true)
+  end
+
+  it 'does not recognize other methods' do
+    expect(Eksek.respond_to?(:some_other_method!)).to be(false)
   end
 end
 
-RSpec.describe Eksek, '#method_missing' do
-  it 'responds to combinations of :stdout/:stderr' do
+RSpec.describe 'Eksek#stdout, Eksek#stderr' do
+  it 'captures the stdout and stderr separatedly' do
     expect(Eksek.stdout('echo Hello')).to eq('Hello')
     expect(Eksek.stderr('echo Hello >&2')).to eq('Hello')
 
@@ -43,14 +59,31 @@ RSpec.describe Eksek, '#method_missing' do
     errout = Eksek.stderr_stdout('echo Hello; echo World >&2')
     expect(errout).to eq(%w[World Hello])
   end
+end
 
+RSpec.describe 'Eksek#stdouterr' do
+  it 'captures the stdout and stderr together' do
+    outerr = Eksek.stdouterr('echo Hello; echo World >&2')
+    expect(outerr).to eq("Hello\nWorld")
+  end
+end
+
+RSpec.describe 'Standard input' do
   it 'accepts a block where the stdin can be written to' do
     o = Eksek.stdout('read A B; echo $A, $B') { |i| i.write('Hi world') }
     expect(o).to eq('Hi, world')
   end
 
-  it 'reads any String or IO into the stdin' do
+  it 'reads a String that the block returns' do
     o = Eksek.stdout('read A; echo $A') { 'Hello' }
     expect(o).to eq('Hello')
+  end
+
+  it 'reads an IO that the block returns' do
+    Tempfile.open do |f|
+      f.write('Hello')
+      o = Eksek.stdout("cat #{f.path}") { f }
+      expect(o).to eq('Hello')
+    end
   end
 end
