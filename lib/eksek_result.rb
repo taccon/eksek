156378be7d17096a233c8502f096179604ed1ea6
Thomas Bretzke <thomas@taccon.com>
2018-08-29 22:54:56 +0200

    feat: added `capture` option and simplified spec

--- lib/eksek_result.rb
+++ lib/eksek_result.rb
@@ -6,20 +6,27 @@ require_relative 'eksek_error'
 # return values from a command
 class EksekResult
   # rubocop:disable Metrics/ParameterLists
-  def initialize env, cmd, exit_code, success, stdout, stderr
+  def initialize env, cmd, exit_code, out_stream, err_stream
     @env = env
     @cmd = cmd
     @exit_code = exit_code
-    @success = success
-    @stdout = stdout
-    @stderr = stderr
+    @out_stream = out_stream
+    @err_stream = err_stream
   end
   # rubocop:enable Metrics/ParameterLists
 
-  attr_reader :exit_code, :stdout, :stderr
+  attr_reader :exit_code
+
+  def stdout
+    @stdout ||= @out_stream&.read
+  end
+
+  def stderr
+    @stderr ||= @err_stream&.read
+  end
 
   def success?
-    @success
+    @exit_code.zero?
   end
 
   def success!
--- spec/eksek_result_spec.rb
+++ spec/eksek_result_spec.rb
@@ -4,8 +4,8 @@ require 'eksekuter'
 
 RSpec.describe 'EksekResult#to_s' do
   it 'returns the stdout' do
-    command = 'echo HelloStdout && echo HelloStderr >&2'
-    output = "The output was: #{Eksekuter.new.run(command)}."
+    command = 'printf HelloStdout && printf HelloStderr >&2'
+    output = "The output was: #{Eksekuter.new.run(command, capture: true)}."
     expect(output).to eq('The output was: HelloStdout.')
   end
 end
